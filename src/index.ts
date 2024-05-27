9e768aff4b28f7555fbec337919d2ed6044a02f6
Cord Hamrick <cordh@cordsworks.com>
2024-05-26 23:17:49 -0400

    Created stub for service with the traditional "Hello, World!";

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,30 @@
-// src/index.ts
-const favoriteFruits: string[] = ["apple", "strawberry", "orange"];
+import * as Koa from "koa";
+import * as Router from "koa-router";
 
-function addFruit(fruit: string) {
-  favoriteFruits.push(fruit);
-}
+import * as logger from "koa-logger";
+import * as json from "koa-json";
+
+const app = new Koa();
+const router = new Router();
+
+// Hello, World!
+router.get("/", async (ctx, next) => {
+  ctx.body = {
+    msg: "Hello, World!"
+  };
+
+  await next();
+});
+
+// Middlewares
+app.use(json());
+app.use(logger());
+
+// Routes
+app.use(router.routes())
+  .use(router.allowedMethods());
+
+// Start Service
+app.listen(3000, () => {
+  console.log("Service Startup");
+});
