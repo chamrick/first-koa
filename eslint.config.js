b6d3c9e3ed782480e42b88b7cf741b88f7ca3225
Cord Hamrick <cordh@cordsworks.com>
2024-05-26 23:07:28 -0400

    Enhanced eslint.config.js;

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,14 @@
-// @ts-check
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReactConfig from "eslint-plugin-react/configs/recommended.js";
+
 
 export default tseslint.config(
-  eslint.configs.recommended,
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
+  pluginReactConfig,
 );
